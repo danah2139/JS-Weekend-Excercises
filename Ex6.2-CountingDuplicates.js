9d4644117938853574c6efbb39c1a5ecbf8a676e
dana cohen <danah2139@gmail.com>
2021-02-20 21:55:06 +0200

    fix the return to an object

--- Ex6.2-CountingDuplicates.js
+++ Ex6.2-CountingDuplicates.js
@@ -5,18 +5,24 @@ const countDuplicates = (str) => {
 	arr.forEach((character) => {
 		character = character.toLowerCase();
 		if (characters.has(character))
-			characters.set(character, characters.get(character + 1));
+			characters.set(character, characters.get(character) + 1);
 		else {
 			characters.set(character, 1);
 		}
 	});
-	let max = 0;
-	let maxKey = '';
+	let max = {};
+	max['no characters repeats more than once'] = 1;
+	//max.value = 1;
 
-	for (const [key, value] of counter) {
-		if (value > max) {
-			max = value;
-			maxKey = key;
+	for (const [key, value] of characters) {
+		if (value > Object.values(max)) {
+			max = {};
+			max[key.toString()] = value;
+		} else if (value == Object.values(max) && value !== 1) {
+			max[key.toString()] = value;
 		}
 	}
+	return max;
 };
+
+console.log(countDuplicates('aabbcde'));
